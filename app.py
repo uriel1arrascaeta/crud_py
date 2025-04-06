09c9da9c66070f250f2e6f531b11e6a76ecd495a
Heber Uriel <hurielarrascaeta@gmail.com>
2025-04-06 16:16:38 -0300

    conexion db 01

--- app.py
+++ app.py
@@ -4,9 +4,24 @@ from flaskext.mysql import MySQL  # conexion a base de datos
 
 app = Flask(__name__)
 
+mysql = MySQL()
+# le estamos para que se conecte a la base de datos mysql vamos utilizar localhost
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'sistema'
+mysql.init_app(app)  # iniciamos
+
 
 @app.route('/')  # ruta a index
 def index():
+
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'oscar', 'perezoscar@gmail.com', 'foto.jpg');"
+    conn = mysql.connect()  # conectamos al index con mysql init
+    cursor = conn.cursor()  # info donde vamos a guardar todo lo que vamos a ejecutar
+    cursor.execute(sql)  # ejecutamos
+    conn.commit()  # aca decimos que la instruccion se termino
+
     return render_template('empleados/index.html')
 
 
