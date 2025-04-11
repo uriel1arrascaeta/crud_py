77c3bf8f718e26c28fb724dabdecf1a0438f547a
Heber Uriel <hurielarrascaeta@gmail.com>
2025-04-11 19:52:58 -0300

    subida de datos a la bd

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from flask import Flask
 from flaskext.mysql import MySQL  # conexion a base de datos
 
@@ -20,6 +20,30 @@ def index():
     conn = mysql.connect()  # conectamos al index con mysql init
     cursor = conn.cursor()  # info donde vamos a guardar todo lo que vamos a ejecutar
     cursor.execute(sql)  # ejecutamos
+    conn.commit()  # aca decimos que la instruccion se termino
+
+    return render_template('empleados/index.html')
+
+
+@app.route('/create')  # muestro el create
+def create():
+    return render_template('empleados/create.html')
+
+
+@app.route('/store', methods=['POST'])
+def storage():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+
+    _foto = request.files['txtFoto']
+
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+
+    datos = (_nombre, _correo, _foto.filename)
+
+    conn = mysql.connect()  # conectamos al index con mysql init
+    cursor = conn.cursor()  # info donde vamos a guardar todo lo que vamos a ejecutar
+    cursor.execute(sql, datos)  # ejecutamos
     conn.commit()  # aca decimos que la instruccion se termino
 
     return render_template('empleados/index.html')
